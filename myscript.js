2b977856741f5d71ef2c0b343d1182231520a8c8
StefanoSerafini <stefanoserafini7@gmail.com>
2020-11-06 17:20:00 +0100

    init

---
+++ myscript.js
@@ -0,0 +1,11 @@
+//Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+//BONUS aggiungere un'immagine presa anch'essa da un data (url dell'immagine)
+
+
+const first = new Vue({
+  el: '$first',
+  data: {
+    title : 'First use',
+    img : ''
+  }
+});
